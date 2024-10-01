c0d391d2de16478d3a2a1325a0fc894752cd67ba
Moraes <moraescodes@gmail.com>
2024-10-01 16:50:37 -0300

    add media queries até 480px

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 const swiper = new Swiper('.swiper', {
   slidesPerView: 4,
   spaceBetween: 20,
@@ -18,6 +16,28 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.botao-esquerda',      // Para o botão "anterior"
   },
 
+  breakpoints: {
+    // Quando a tela for >= 320px (telas pequenas)
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // Quando a tela for >= 480px (telas médias)
+    420: {
+      slidesPerView: 2,
+      spaceBetween: 15
+    },
+    // Quando a tela for >= 768px (telas maiores)
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    // Quando a tela for >= 1024px (telas grandes)
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    }
+  }
 
 });
 $(document).ready(function () {
@@ -45,7 +65,35 @@ $(document).ready(function () {
       nextEl: '.botao-direita',
       prevEl: '.botao-esquerda',
     },
+    breakpoints: {
+      // Quando a tela for >= 320px (telas pequenas)
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      // Quando a tela for >= 480px (telas médias)
+      420: {
+        slidesPerView: 2,
+        spaceBetween: 15
+      },
+      // Quando a tela for >= 768px (telas maiores)
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      // Quando a tela for >= 1024px (telas grandes)
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      }
+    }
   });
+  swiperVitrine.find('.botao-comprar').each(function () {
+    $(this).on('click', function () {
+      showModal();  // Abre o modal ao clicar em um botão "comprar"
+    });
+  });
+  
 });
 
 // Seleciona o modal e o botão de fechar
@@ -54,28 +102,29 @@ const closeModal = document.querySelector(".close");
 
 // Função para mostrar o modal
 function showModal() {
-    modal.style.display = "block"; // Mostra o modal
+  modal.style.display = "block"; // Mostra o modal
 }
 
 // Função para fechar o modal
 function hideModal() {
-    modal.style.display = "none"; // Fecha o modal
+  modal.style.display = "none"; // Fecha o modal
 }
 
 // Fecha o modal se o usuário clicar fora dele
-window.onclick = function(event) {
-    if (event.target === modal) {
-        hideModal();
-    }
+window.onclick = function (event) {
+  if (event.target === modal) {
+    hideModal();
+  }
 }
 
 // Adiciona evento de clique a todos os botões de comprar
 const buyButtons = document.querySelectorAll('.botao-comprar');
 buyButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        showModal();
-    });
+  button.addEventListener('click', function () {
+    showModal();
+  });
 });
 
 // Evento para fechar o modal
 closeModal.addEventListener('click', hideModal);
+
